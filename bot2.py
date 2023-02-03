0796c987e20ccd6c89189ff60a2ecbc88cce488c
Enric Cervera <ecervera@uji.es>
2023-02-03 14:59:27 +0100

    add kernel

--- bot2.py
+++ bot2.py
@@ -2,6 +2,9 @@ import logging, sys
 from telegram import Update
 from telegram.ext import filters, MessageHandler, ApplicationBuilder, ContextTypes, CommandHandler
 
+import jupyter_client
+from Listener import Listener
+
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
     level=logging.INFO
@@ -11,7 +14,13 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
 
 async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text)
+	global kernel_client
+	li = Listener()
+	reply = kernel_client.execute_interactive(update.message.text, timeout=5.0, 
+										  allow_stdin=False, 
+                                          output_hook=li.output_cb)
+	if li.text:                                      
+		await context.bot.send_message(chat_id=update.effective_chat.id, text=li.text)
 
 if __name__ == '__main__':
     if len(sys.argv) < 2:
@@ -20,6 +29,8 @@ if __name__ == '__main__':
     else:
         token = sys.argv[1]
 	
+    kernel_manager, kernel_client = jupyter_client.manager.start_new_kernel(kernel_name='python3')
+
     application = ApplicationBuilder().token(token).build()
     
     start_handler = CommandHandler('start', start)
